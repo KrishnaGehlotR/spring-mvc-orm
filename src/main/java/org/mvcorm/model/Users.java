d912369b3e54f0e308f29ec5e7b55cd9e9e19351
Krishna Gehlot R <gehlotrkrishna@gmail.com>
2019-06-04 19:37:59 +0530

    Solved 404 error

--- src/main/java/org/mvcorm/model/Users.java
+++ src/main/java/org/mvcorm/model/Users.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Users {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "userId")
 	private int userId;
 	
